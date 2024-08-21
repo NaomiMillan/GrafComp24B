57b60e2fb9ab64b768846a2984643097debafddb
Naomi Millan <144079036+NaomiMillan@users.noreply.github.com>
2024-08-20 20:09:25 -0600

    Delete programa1.py

--- programa1.py
+++
@@ -1,9 +0,0 @@
-from manim import *
-
-class FirstScene(Scene):
-
-    def construct(self):
-        sq = Square()
-        circ = Circle().set_fill(opacity=1)
-        self.play(Transform(sq, circ))
-        self.wait()
