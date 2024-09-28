bc453c7db8c14bdd81c3263db1ed4a328ad41855
Naomi Millan <144079036+NaomiMillan@users.noreply.github.com>
2024-09-28 10:01:41 -0600

    Subir archivos

---
+++ 07_Figuras/Figuras.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Creación de figuras con líneas
+# 
+# ### Trabajo realizado por: Jessica Naomi Millan Sánchez
+# ### Graficación Computacional
+# ### Profesora: Hazem Álvarez Rodríguez
+# ### Clase del 25 de septiembre de 2024
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[5]:
+
+
+plt.plot([1, 1.5, 2, 1], [1, 2, 1, 1])
+
+
+# In[8]:
+
+
+plt.plot([1, 1, 2, 2, 1], [1, 2, 2, 1, 1])
+
+
+# In[9]:
+
+
+plt.plot([1, 1.5, 2, 1], [1, 2, 1, 1])
+plt.plot([1, 1, 2, 2, 1], [1, 2, 2, 1, 1])
+
