112c39f72ce34d7c1f526e864049fa767b9c2d75
Naomi Millan <144079036+NaomiMillan@users.noreply.github.com>
2024-11-23 00:54:37 -0600

    Update PizzaAnimada.py

--- 14_Escenarios_Pizzache/PizzaAnimada.py
+++ 14_Escenarios_Pizzache/PizzaAnimada.py
@@ -1,3 +1,10 @@
+# Escenarios de la historia de Pizzache
+
+### Trabajo realizado por: Jessica Naomi Millan Sánchez
+### Graficación Computacional
+### Profesora: Hazem Álvarez Rodríguez
+### Clase del 06 de noviembre de 2024
+
 from manim import *
 
 class PizzaWithGlasses(Scene):
